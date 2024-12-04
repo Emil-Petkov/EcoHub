ab7b48ba28886815bd049c783bb3a78c67c821d0
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-12-04 03:47:31 +0200

    Implemented random display of top-rated client reviews with distinction between users

--- EcoHub/reviews/models.py
+++ EcoHub/reviews/models.py
@@ -1,9 +1,16 @@
 from django.conf import settings
 from django.db import models
 
+
 class Review(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     comment = models.TextField()
-    satisfaction = models.IntegerField(choices=[(i, str(i)) for i in range(1, 6)])
+    satisfaction = models.PositiveIntegerField()
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
     created_at = models.DateTimeField(auto_now_add=True)
--- EcoHub/reviews/views.py
+++ EcoHub/reviews/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render, redirect
 from .models import Review
 from .forms import ReviewForm
+import random
+
 
 def testimonial_view(request):
     if request.method == 'POST':
-        form = ReviewForm(request.POST, request.FILES)
+        form = ReviewForm(request.POST)
         if form.is_valid():
             review = form.save(commit=False)
             if request.user.is_authenticated:
                 review.user = request.user
             review.save()
             return redirect('testimonial')
-        else:
-            print(form.errors)
     else:
         form = ReviewForm()
 
-    reviews = Review.objects.all().order_by('-created_at')
+    all_reviews = (
+        Review.objects.filter(satisfaction__gte=4)
+        .order_by('first_name', 'last_name', '-created_at')
+        .distinct('first_name', 'last_name')
+    )
+
+    reviews = random.sample(list(all_reviews), min(len(all_reviews), 5))
+
     return render(request, 'reviews/testimonial.html', {'form': form, 'reviews': reviews})
