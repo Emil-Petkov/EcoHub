c46aa2b7f95ab37203c895871f4190e02b6f2217
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-12-11 10:48:52 +0200

    Complete Common app

--- EcoHub/common/tests.py
+++ EcoHub/common/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class CommonTests(TestCase):
+
+    def test_index_page(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_contact_page(self):
+        response = self.client.get(reverse('contact'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact.html')
+
+    def test_404_page(self):
+        response = self.client.get('/nonexistent/')
+        self.assertEqual(response.status_code, 404)
+        self.assertTemplateUsed(response, '404/404.html')
